1c8b416e79cd87dc3ac6206fe7cbebf9111bee5b
austinb97 <31427546+austinb97@users.noreply.github.com>
2017-11-27 15:11:33 -0700

    added initial files

---
+++ infoSite/js/main.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+// Scroll Fix Nav 
+var  mn = $(".nav");
+    mns = "nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
